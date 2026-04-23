f7ac8391c8a5734b528d775f50724f763cb6521e
Skeny Antonio <skantoni@c3r1s5.42luanda.com>
2026-04-23 15:04:16 +0100

    Esphere

--- minirt.h
+++ minirt.h
@@ -16,4 +16,30 @@
 #include <unistd.h>
 #include "./includes/minilibx-linux/mlx.h"
 
+typedef struct s_vec3
+{
+    double x;
+    double y;
+    double z;
+} t_vec3;
+
+t_vec3 vec3(double x, double y, double z)
+{
+    t_vec3 v;
+    v.x = x;
+    v.y = y;
+    v.z = z;
+    return v;
+}
+
+t_vec3 sub(t_vec3 a, t_vec3 b)
+{
+    return vec3(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+double dot(t_vec3 a, t_vec3 b)
+{
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
 #endif
--- teste.c
+++ teste.c
@@ -1,5 +1,18 @@
 #include "minirt.h"
 
+int hit_sphere(t_vec3 center, double radius, t_vec3 origin, t_vec3 dir)
+{
+    t_vec3 oc = sub(origin, center);
+
+    double a = dot(dir, dir);
+    double b = 2.0 * dot(oc, dir);
+    double c = dot(oc, oc) - radius * radius;
+
+    double delta = b*b - 4*a*c;
+
+    return (delta > 0);
+}
+
 int main(void)
 {
     int x, y, width, height;
@@ -18,13 +31,18 @@ int main(void)
         x = 0;
         while (x < width)
         {
-            r = (double)x / (width - 1);
-            g = (double)y / (height - 1);
-            b = 0.0;
-            //if ((x >= 75 && x <= 150) && (y >= 25 && y <= 75))
-                printf("%d %d %d ", (int)(255*r), (int)(255*g), (int)(255*b));
-            //else
-              //  printf("0 0 0 ");
+            double u = (double)x / (width - 1);
+            double v = (double)y / (height - 1);
+            double px = 2 * u - 1;
+            double py = 2 * v - 1;
+
+            t_vec3 origin = vec3(0,0,0);
+            t_vec3 dir = vec3(px, py, -1);
+
+            if (hit_sphere(vec3(0,0,-1), 0.5, origin, dir))
+                printf("255 0 0 ");
+            else
+                  printf("0 0 0 ");
             x++;
         }
         y++;
