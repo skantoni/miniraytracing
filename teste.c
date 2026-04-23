0633cee583239f65970b86d58aa2b94b25fe731f
Skeny Antonio <skantoni@c3r1s5.42luanda.com>
2026-04-23 13:10:53 +0100

    Apredi a pintar gradiante usando valores resai(Decimais(Com virgulas(Nao inteiros)))

--- teste.c
+++ teste.c
@@ -2,23 +2,29 @@
 
 int main(void)
 {
-    int x, y, width, heigth;
+    int x, y, width, height;
+    double  r;
+    double  g;
+    double  b;
 
     x = 0;
     y = 0;
     width = 200;
-    heigth = 100;
+    height = 100;
 
-    printf("P3\n%d %d\n255\n", width, heigth);
-    while (y < heigth)
+    printf("P3\n%d %d\n255\n", width, height);
+    while (y < height)
     {
         x = 0;
         while (x < width)
         {
-            if ((x >= 75 && x <= 150) && (y >= 25 && y <= 75))
-                printf("0 0 255 ");
-            else
-                printf("0 0 0 ");
+            r = (double)x / (width - 1);
+            g = (double)y / (height - 1);
+            b = 0.0;
+            //if ((x >= 75 && x <= 150) && (y >= 25 && y <= 75))
+                printf("%d %d %d ", (int)(255*r), (int)(255*g), (int)(255*b));
+            //else
+              //  printf("0 0 0 ");
             x++;
         }
         y++;
